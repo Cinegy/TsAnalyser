1a6e2bb85df4a053e21f371f85809fb01806e16b
Lewis Kirkaldie <lewis@cinegy.com>
2016-10-27 11:32:10 +0100

    -added missed Files -moved LICENSE, readme and git files

---
+++ TsAnalyser/Metrics/SerialisableMetrics.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace TsAnalyser.Metrics
+{
+    public class SerialisableMetrics
+    {
+        public SerialisableMetrics()
+        {
+            Network = new SerialisableNetworkMetric();
+            Ts = new SerialisableTsMetric();
+            Rtp = new SerialisableRtpMetric();
+            Service = new ServiceDescriptionMetric();
+        }
+
+        public SerialisableNetworkMetric Network { get; set; }
+        public SerialisableTsMetric Ts { get; set; }
+
+        public SerialisableRtpMetric Rtp { get; set; }
+        public ServiceDescriptionMetric Service { get; set; }
+
+        public class SerialisableNetworkMetric
+        {
+            public long TotalPacketsRecieved { get; set; }
+            public long CurrentBitrate { get; set; }
+            public long HighestBitrate { get; set; }
+            public long LongestTimeBetweenPackets { get; set; }
+            public long LowestBitrate { get; set; }
+            public float NetworkBufferUsage { get; set; }
+            public int PacketsPerSecond { get; set; }
+            public long ShortestTimeBetweenPackets { get; set; }
+            public long TimeBetweenLastPacket { get; set; }
+            public long AverageBitrate { get; set; }
+        }
+
+        public class SerialisableTsMetric
+        {
+            public SerialisableTsMetric()
+            {
+                Pids = new List<PidDetails>();
+            }
+
+            public List<PidDetails> Pids { get; set; }
+
+            public class PidDetails
+            {
+                public int Pid { get; set; }
+                public long CcErrorCount { get; set; }
+                public long PacketCount { get; set; }
+                public string StreamType { get; set; }
+            }
+        }
+
+        public class SerialisableRtpMetric
+        {
+            public long MinLostPackets { get; set; }
+            public long SequenceNumber { get; set; }
+            public long Timestamp { get; set; }
+            public long Ssrc { get; set; }
+        }
+
+        public class ServiceDescriptionMetric
+        {
+            public string ServiceName { get; set; }
+            public string ServiceProvider { get; set; }
+        }
+    }
+}
---
+++ TsAnalyser/Service/ITsAnalyserApi.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using TsAnalyser.Metrics;
+
+namespace TsAnalyser.Service
+{
+
+    [ServiceContract]
+    public interface ITsAnalyserApi
+    {
+        [OperationContract]
+        [WebInvoke(Method = "OPTIONS", UriTemplate = "/*")]
+        void GetGlobalOptions();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/*")]
+        Stream ServeEmbeddedStaticFile();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "")]
+        Stream ServeIndexEmbeddedStaticFile();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/V1/CurrentMetrics")]
+        SerialisableMetrics GetCurrentMetrics();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/V1/ResetMetrics")]
+        void ResetMetrics();
+        
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/V1/Start")]
+        void StartStream();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/V1/Stop")]
+        void StopStream();
+
+    }
+
+
+}
