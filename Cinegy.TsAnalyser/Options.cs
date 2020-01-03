2fde2fa5cc07b50832a25eb1377173398b68eb34
Lewis Kirkaldie <lewis@cinegy.com>
2020-01-03 12:45:36 +0000

    -lowercase binary name -update analysis lib (bitrates fix)

--- Cinegy.TsAnalyser/Options.cs
+++ Cinegy.TsAnalyser/Options.cs
@@ -7,10 +7,6 @@ namespace Cinegy.TsAnalyser
         [Option('q', "quiet", Required = false, Default = false,
         HelpText = "Don't print anything to the console")]
         public bool SuppressOutput { get; set; }
-
-        //[Option('l', "logfile", Required = false,
-        //HelpText = "Optional file to record events to.")]
-        //public string LogFile { get; set; }
     
         [Option('s', "skipdecodetransportstream", Required = false, Default = false,
         HelpText = "Optional instruction to skip decoding further TS and DVB data and metadata")]
@@ -61,18 +57,6 @@ namespace Cinegy.TsAnalyser
         HelpText = "Optional instruction to skip the expected 12 byte RTP headers (meaning plain MPEGTS inside UDP is expected")]
         public bool NoRtpHeaders { get; set; }
         
-        //[Option('i', "interarrivaltime", Required = false, Default = 40,
-        //HelpText = "Maximum permitted time between UDP packets before alarming.")]
-        //public int InterArrivalTimeMax { get; set; }
-        
-        //[Option('h', "savehistoricaldata", Required = false, Default = false,
-        //HelpText = "Optional instruction to save and then flush to disk recent TS data on stream problems.")]
-        //public bool SaveHistoricalData { get; set; }
-
-        //[Option('e', "timeserieslogging", Required = false,
-        //HelpText = "Record time slice metric data to log file.")]
-        //public bool TimeSeriesLogging { get; set; }
-
     }
 
     // Define a class to receive parsed values read verb
